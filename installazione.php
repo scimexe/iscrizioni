c37fd41eb9ecdb62c8b98e915013c7d720f93fb4
Andrea Scimeca <78423346+scimexe@users.noreply.github.com>
2021-11-11 00:50:35 +0100

    Add files via upload

---
+++ installazione.php
@@ -0,0 +1,47 @@
+<?php
+	include "/var/www/html/newCDB.php";
+	
+	$inst=new CDB();
+	$r=$inst->test();
+	
+	$inst->fcreaCitta();
+	if($r=$inst->fcreaCitta()){
+		echo "Errore tabella Città<br>".$r;
+		return "";
+	}
+	$inst->fcreaRuoli();
+	if($r=$inst->fcreaRuoli()){
+		echo "Errore tabella Ruoli<br>".$r;
+		return "";
+	}
+	$inst->fcreaNazioni();
+	if($r=$inst->fcreaNazioni()){
+		echo "Errore tabella Nazioni<br>".$r;
+		return "";
+	}
+	$inst->fcreaComponenti();
+	if($r=$inst->fcreaComponenti()){
+		echo "Errore tabella Componenti<br>".$r;
+		return "";
+	}
+	$inst->fcreaAnagrafiche();
+	if($r=$inst->fcreaAnagrafiche()){
+		echo "Errore tabella Anagrafiche<br>".$r;
+		return "";
+	}
+	$inst->fcreaAccount();
+	if($r=$inst->fcreaAccount()){
+		echo "Errore tabella Account<br>".$r;
+		return "";
+	}
+	$inst->fcreaCittadinanze();
+	if($r=$inst->fcreaCittadinanze()){
+		echo "Errore tabella Cittadinanze<br>".$r;
+		return "";
+	}
+	$inst->fcreaRuoliAnagrafica();
+	if($r=$inst->fcreaRuoliAnagrafica()){
+		echo "Errore tabella RuoliAnagrafica<br>".$r;
+		return "";
+	}	
+?>	
---
+++ newCDB.php
@@ -0,0 +1,184 @@
+<?php
+
+	class CDB{
+		private $host='127.0.0.1';
+		private $user='andrea';
+		private $passwd='root';
+		private $nomeDB='inserzioni';
+		private $conn;
+
+        public function connessione(){
+            $this->conn = new mysqli($this->$host, $this->$user, $this->$passwd, $this->nomeDB);
+            if($this->conn->connect_error){
+                die("Connessione fallita: " . $conn->connect_error);
+            }
+        }
+
+        private function connessioneROOT(){
+			$this->conn= new mysqli($this->host,$this->user,$this->passwd);
+			if($this->conn->connect_error){
+                die("Connessione fallita: " . $conn->connect_error);
+            }
+        }
+
+        private function creaDB(){
+            $this->connessioneROOT();
+			$this->conn->query("CREATE DATABASE ".$this->nomeDB);
+			$r=$this->conn->error;
+			$this->conn->close();
+			return $r;
+		}
+
+        public function test(){
+            return $this->creaDB();
+        }
+
+        public function fCreaCitta(){
+            $sql="CREATE TABLE IF NOT EXISTS $this->nomeDB.tbCitta(
+				idCodice CHAR(5) NOT NULL,
+				PRIMARY KEY (idCodice),
+				denominazione VARCHAR(100) NOT NULL);
+            ";    
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaRuoli(){
+            $sql="CREATE TABLE IF NOT EXISTS $this->nomeDB.tbRuoli(
+                idRuolo INT NOT NULL,
+				PRIMARY KEY (idRuolo),
+				ruolo VARCHAR (50) NOT NULL);
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaNazioni(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbNazioni(
+                idNazione CHAR (10),
+                PRIMARY KEY (idNazione),
+                denominazione VARCHAR (100) NOT NULL,
+                cittadinanza VARCHAR (100) NOT NULL);
+            ";    
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaComponenti(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbComponenti(
+				idComponente INT NOT NULL AUTO_INCREMENT,
+				PRIMARY KEY (idComponente),
+				denominazione VARCHAR (100) NOT NULL);
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaAnagrafiche(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbAnagrafica(
+                idAnagrafica BIGINT NOT NULL AUTO_INCREMENT,
+                PRIMARY KEY(idAnagrafica),
+                nome VARCHAR (100) NOT NULL,
+                cognome VARCHAR (100) NOT NULL,
+                dataNascita DATE NOT NULL,
+                codiceFiscale CHAR (16) NOT NULL UNIQUE,
+                extCittaNascita CHAR (5) NOT NULL,
+                FOREIGN KEY(extCittaNascita) REFERENCES tbCitta(idCodice),
+                extNazione CHAR (10) NOT NULL,
+                FOREIGN KEY(extNazione) REFERENCES tbNazioni(idNazione),
+                extComponente INT NOT NULL,
+                FOREIGN KEY(extComponente) REFERENCES tbComponenti(idComponente),
+                inserimento TIMESTAMP NOT NULL);
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaAccount(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbAccount(
+				user VARCHAR (100) NOT NULL,
+                PRIMARY KEY (user),
+				passwd VARCHAR (32) NOT NULL,
+				livello INT NOT NULL,
+                extAnagrafica BIGINT,
+				FOREIGN KEY(extAnagrafica) REFERENCES tbAnagrafica(idAnagrafica));
+				
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }
+
+        public function fcreaCittadinanze(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbCittadinanze(
+				extAnagrafica BIGINT NOT NULL,
+                FOREIGN KEY(extAnagrafica) REFERENCES tbAnagrafica(idAnagrafica),
+				extNazione CHAR (5) NOT NULL,
+                FOREIGN KEY(extNazione) REFERENCES tbNazioni(idNazione));
+                
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+        }   
+
+        public function fcreaRuoliAnagrafica(){
+            $sql="CREATE TABLE IF NOT EXISTS inserzioni.tbRuoliAnagrafica(
+				extAnagrafica BIGINT NOT NULL,
+				extRuoli INT NOT NULL,
+                FOREIGN KEY (extAnagrafica) REFERENCES tbAnagrafica(idAnagrafica),
+                FOREIGN KEY (extRuoli) REFERENCES tbRuoli(idRuolo));
+
+            ";
+            $this->connessioneROOT();
+            if($this->conn->query($sql) == null){
+                return $this->conn->error;
+            }
+            else{
+                return "";
+            }
+            $this->conn->close();
+
+        } 
+    }
+?>
